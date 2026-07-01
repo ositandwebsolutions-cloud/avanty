b012fc0fd19aea1a8a466eb88dffcc369b7bf81c
Your Name <orpon.pciu@gmail.com.com>
2026-07-01 19:18:16 +0600

    Add new edits

--- resources/views/frontend/home/about-us.blade.php
+++ resources/views/frontend/home/about-us.blade.php
@@ -113,11 +113,11 @@
                 
                 <div class="team-card animate-on-scroll delay-3" data-member="kai">
                     <div class="team-img-wrapper">
-                        <img src="{{ asset('assets/frontend/img/lauis.jpg') }}" alt="Kai Yung Laui" class="team-img">
+                        <img src="{{ asset('assets/frontend/img/lauis.jpg') }}" alt="Mrs. Kai Leuw" class="team-img">
                         <div class="team-gradient"></div>
                     </div>
                     <div class="team-info">
-                        <h3 class="team-name">Kai Yung Laui</h3>
+                        <h3 class="team-name">Mrs. Kai Leuw</h3>
                         <p class="team-title">Head of Operations</p>
                         <a href="#" class="bio-btn">
                             <span>Read Biography</span>
