8d13158555c5ad15d8c1dedbdee84372d4e829e9
Your Name <orpon.pciu@gmail.com.com>
2026-07-03 23:00:14 +0600

    Add new edits addedg now for see more an ghero image of about us

--- resources/views/frontend/home/capabilities.blade.php
+++ resources/views/frontend/home/capabilities.blade.php
@@ -7,7 +7,7 @@
     
     @include('frontend.home.css')
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/frontend/css/capabilities.css') }}?v=1.3">
+    <link rel="stylesheet" href="{{ asset('assets/frontend/css/capabilities.css') }}?v=1.5">
 </head>
 <body>
 
