4cefca12a008a61295272c49dd84fbc9ecc2bff3
Your Name <orpon.pciu@gmail.com.com>
2026-07-06 21:29:32 +0600

    Add new edits addedg now for see more an ghero image of about us 16th itmeadmin update

--- resources/views/frontend/home/css.blade.php
+++ resources/views/frontend/home/css.blade.php
@@ -1,3 +1,3 @@
 <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="icon" type="image/jpeg" href="/assets/frontend/img/favicon1.png">
