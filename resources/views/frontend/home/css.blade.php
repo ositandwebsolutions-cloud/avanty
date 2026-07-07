7049b8a823f7c2f2ecb444b1fe2a504992973307
Your Name <orpon.pciu@gmail.com.com>
2026-07-08 00:55:27 +0600

    Add new edits addedg now for see more an ghero image of about us 16th itmeadmin update

--- resources/views/frontend/home/css.blade.php
+++ resources/views/frontend/home/css.blade.php
@@ -1,3 +1,6 @@
+<title>Avanty Capital - Empowering Legacy</title>
+<meta name="description" content="Avanty Capital operates alongside its own family office interests and a trusted network of external family offices, institutional participants, high net worth individuals and corporate stakeholders.">
+
 <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/jpeg" href="/assets/frontend/img/favicon1.png">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
+<link rel="icon" type="image/jpeg" href="/assets/frontend/img/favicon1.png">
