c3f8c5e966f477909d1d829a3dc8a66cdcb3b578
Your Name <orpon.pciu@gmail.com.com>
2026-07-23 06:37:22 +0600

    Add new edits addedg now for see more an ghero image of about us 16th itmeadmin update new something in header of book  a table and so many things

--- resources/views/frontend/home/status.blade.php
+++ resources/views/frontend/home/status.blade.php
@@ -1,23 +1,22 @@
 <div class="stats-bar">
-            <div class="container">
-                <div class="stats-grid">
-                    <div class="stat-item">
-                        <div class="stat-number">$800M+</div>
-                        <div class="stat-text">Capital Origination</div>
-                    </div>
-                    <div class="stat-item">
-                        <div class="stat-number">Strategy Compounded</div>
-                        <div class="stat-text">Family Office & Principal <span class="light">Mindset</span></div>
-                    </div>
-                    <div class="stat-item">
-                        <div class="stat-number">Asia • Middle East • Europe</div>
-                        <div class="stat-text">Cross Border <span class="light">Transactions</span></div>
-                    </div>
-                    <div class="stat-item">
-                        <div class="stat-number">Focused on Execution</div>
-                        <div class="stat-text"><span class="light">Disciplined • Trusted • Aligned</span></div>
-                    </div>
-                </div>
+    <div class="container">
+        <div class="stats-grid">
+            <div class="stat-item">
+                <div class="stat-number">$800M+</div>
+                <div class="stat-text">Capital Origination</div>
+            </div>
+            <div class="stat-item">
+                <div class="stat-number">Strategy Compounded</div>
+                <div class="stat-text">Family Office & Principal</div>
+            </div>
+            <div class="stat-item">
+                <div class="stat-number">Asia • Middle East • Europe</div>
+                <div class="stat-text">Cross Border <span class="light">Transactions</span></div>
+            </div>
+            <div class="stat-item">
+                <div class="stat-number">Focused on Execution</div>
+                <div class="stat-text"><span class="light">Disciplined • Trusted • Aligned</span></div>
             </div>
         </div>
     </div>
+</div>
